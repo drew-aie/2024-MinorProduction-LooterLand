8e8e65bb92022a21cc5e2a232f4dbd7316e127fa
Jurhell Hughes <somethinghughes@gmail.com>
2024-05-06 13:13:50 -0500

    feat: Added Timer UI and TimerBehavior script

---
+++ Assets/_Project/Gameplay/UI/TimerBehavior.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TimerBehavior : MonoBehaviour
+{
+    [Tooltip("The Textmesh that will display the timer countdown to the player.")]
+    [SerializeField]
+    private TextMeshProUGUI _timerDisplay;
+
+    [Space]
+
+    [Tooltip("The length of the countdown in minutes")]
+    [SerializeField, Min(0)]
+    private int _minutes = 1;
+
+    [Tooltip("The length of the countdown in seconds")]
+    [SerializeField, Min(0)]
+    private int _seconds = 0;
+
+    private float _countDown;
+
+    void Start()
+    {
+        //Guard clause that caps seconds at 60
+        if (_seconds >= 60)
+            _seconds = 60;
+
+        //Multiplies minutes value by 60 and adds the seconds value to it
+        _countDown = (60 * _minutes) + _seconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Guard clause that won't run update without a timer display
+        if (!_timerDisplay)
+            return;
+
+        //Countdown decrements with delta time each update call
+        _countDown -= Time.deltaTime;
+
+        //Converts countdown value into minutes and seconds
+        int minutes = Mathf.FloorToInt(_countDown / 60);
+        int seconds = Mathf.FloorToInt(_countDown % 60);
+
+        //Displays the minutes and seconds in the timer ui
+        _timerDisplay.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
