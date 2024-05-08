f0480cf50858dd1793864a7e53e3f47e91d89ee3
Justin Judice <jjudice51@gmail.com>
2024-05-08 12:52:50 -0500

    fix: Updated Input class with comments
    also deleted unused code from previous implementation. when movement was still not functioning as intended

--- Assets/_Project/Gameplay/PlayerController/Input.cs
+++ Assets/_Project/Gameplay/PlayerController/Input.cs
@@ -5,18 +5,26 @@ using UnityEngine.InputSystem;
 
 public class Input : MonoBehaviour
 {
+    //the main vector 2 for movement
     private Vector2 _locomotionInput;
 
     
+    //how many units a character moves per second of input in a direction
     [SerializeField]
     private float _speed;
+
+    //the Action map used for input.cs
     private PlayerControls _playerActions;
+
+    //the player's rigidbody
     private Rigidbody _playerRigidbody;
 
     private void Awake()
     {
+        //defines _playerActions before start is called.
         _playerActions = new PlayerControls();
 
+        //defines _playerRigidbody before start is called. if the rigidbody component is null then the debug log will alert of it.
         _playerRigidbody = GetComponent<Rigidbody>();
         if (_playerRigidbody == null)
             Debug.LogError("Rigidbody is NULL");
@@ -24,26 +32,25 @@ public class Input : MonoBehaviour
 
     private void OnEnable()
     {
+        //when OnEnable is called it enables this action map
         _playerActions.Locomotion.Enable();
     }
 
     private void OnDisable()
     {
+        //when OnDisable is called it disables this action map
         _playerActions.Locomotion.Disable();
     }
-    //when this function is called it will take in the context of the action and make that the locomotionInput the engine takes in.
- //   public void OnMove(InputAction.CallbackContext context)
- //   {
-    //    _locomotionInput = context.action.ReadValue<Vector2>();
-     //   print(_locomotionInput);
-   //     _playerRigidbody.velocity = _locomotionInput * _speed;
-   // }
-
+   
     private void FixedUpdate()
     {
-
+        //makes locomotion input = to whatever direction the action map says it is based on a 2D X,Y axis
         _locomotionInput = _playerActions.Locomotion.Move.ReadValue<Vector2>();
+
+        //takes that Vector 2 we created previously and uses that information to make a vector 3 for our input direction
         Vector3 move = new Vector3(_locomotionInput.x, 0, _locomotionInput.y);
+
+        //makes the player's velocity = to the vector 3 move direction * our speed value.
         _playerRigidbody.velocity = move * _speed;
     }
 
