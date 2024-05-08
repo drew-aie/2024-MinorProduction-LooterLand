920bba01ed543e40cddac36353b6140c3d6eab0b
Jurhell Hughes <somethinghughes@gmail.com>
2024-05-08 17:00:42 -0500

    feat: Created patrol points for patrol behavior.

--- Assets/_Project/Characters/Cop/CopPatrolBehavior.cs
+++ Assets/_Project/Characters/Cop/CopPatrolBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,25 +7,55 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class CopPatrolBehavior : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] _navPoints;
+
     private NavMeshAgent _cop;
+    private NavMeshPath _path;
     private EState _currentState = EState.IDLE;
 
-    //
+    private int _navIter;
+
+    private bool _patrolStarted = false;
+    private bool _hasReachedPath = true;
+
+    //Enum holding behavior states
     enum EState
     {
         IDLE,
+        PATROL,
         WANDER,
         END 
     };
 
-    private void TransitionTo(EState state)
+    void StateMachine()
     {
-        //Guard Clause
-        if (state == EState.END || state == _currentState)
-            return;
+        //switch (_currentState)
+        //{
+        //    case EState.IDLE:
+        //        //Check state 
+        //        if (_currentState != EState.IDLE)
+        //            break;
 
-        _currentState = state;
+        //        break;
 
+        //    case EState.WANDER:
+        //        //Check state
+        //        if (_currentState != EState.WANDER)
+        //            break;
+
+        //        break;
+
+        //    case EState.END:
+        //        //Check state
+        //        if (_currentState != EState.END)
+        //            break;
+
+        //        break;
+
+        //    default:
+        //        break;
+        //}
     }
 
     // Start is called before the first frame update
@@ -36,31 +67,60 @@ public class CopPatrolBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (_currentState)
+        if (_currentState == EState.IDLE)
         {
-            case EState.IDLE:
-                //Check state 
-                if (_currentState != EState.IDLE)
-                    break;
-
-                break;
-
-            case EState.WANDER:
-                //Check state
-                if (_currentState != EState.WANDER)
-                    break;
-
-                break;
-
-            case EState.END:
-                //Check state
-                if (_currentState != EState.END)
-                    break;
-
-                break;
-
-            default:
-                break;
+            TransitionTo(EState.PATROL);
+            return;
         }
+        else if (_currentState == EState.PATROL)
+        {
+            if (!_hasReachedPath)
+            PatrolPath();
+            return;
+        }
+        else if (_currentState == EState.WANDER)
+            Wander();
+        else
+            return;
+
+
+    }
+
+    /// <summary>
+    /// Transitions from one state to another.
+    /// </summary>
+    /// <param name="state">The behavior that will become the current state.</param>
+    private void TransitionTo(EState state)
+    {
+        //Guard Clause
+        if (state == EState.END || state == _currentState)
+            return;
+
+        _currentState = state;
+
+    }
+
+    private void PatrolPath()
+    {
+        if (!_patrolStarted)
+        {
+            _navIter = -1;
+            _patrolStarted = true;
+        }
+        _navIter++;
+
+        if (!_hasReachedPath)
+            return;
+
+        _cop.destination = _navPoints[_navIter].transform.position;
+
+        if (_hasReachedPath)
+            TransitionTo(EState.IDLE);
+
+    }
+
+    private void Wander()
+    {
+        //Wander
     }
 }
