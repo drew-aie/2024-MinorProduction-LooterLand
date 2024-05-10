3dec5c1b93bcb58a8634d6bd504fc7512a091d89
Justin Judice <jjudice51@gmail.com>
2024-05-10 12:07:13 -0500

    Began making score system script

---
+++ Assets/_Project/Gameplay/Collectables/PlayerInteraction/ScoreSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+
+    float _currentScore;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if
+    }
+
+
+}
