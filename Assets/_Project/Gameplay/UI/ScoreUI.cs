d1e20d1c9b20c8a2cfa5efebfc85bb75fb591b42
Jurhell Hughes <somethinghughes@gmail.com>
2024-05-13 14:51:47 -0500

    feat: Finished player cash UI

--- Assets/_Project/Gameplay/UI/ScoreUI.cs
+++ Assets/_Project/Gameplay/UI/ScoreUI.cs
@@ -9,17 +9,18 @@ public class ScoreUI : MonoBehaviour
     [SerializeField, Tooltip("The TextMesh that will display the player's score in the UI.")]
     private TextMeshProUGUI _scoreDisplay;
 
-    //private ScoreSystem _scoreSystem;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Tooltip("The player's Score System Component, holds the cash amount that will go into Score Display. " +
+        "Can't be set in UI prefab, must be set in scene.")]
+    [SerializeField]
+    private ScoreSystem _playerScore;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!_scoreDisplay || !_playerScore)
+            return;
+
+        _scoreDisplay.text = "$" + _playerScore.CurrentScore;
+        Debug.Log(_playerScore.CurrentScore);
     }
 }
