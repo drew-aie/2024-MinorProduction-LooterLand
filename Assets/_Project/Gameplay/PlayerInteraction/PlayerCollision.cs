008e0a7ce6fd3ed5c0e37ed48327635e9d03eca9
Christian Deese <s238007@students.aie.edu>
2024-05-20 09:23:55 -0500

    fix: player didn't lose cash on collision.

--- Assets/_Project/Gameplay/PlayerInteraction/PlayerCollision.cs
+++ Assets/_Project/Gameplay/PlayerInteraction/PlayerCollision.cs
@@ -7,46 +7,36 @@ public class PlayerCollision : MonoBehaviour
     [SerializeField]
     [Tooltip("The Manager that keeps track of increasing and decreasing of player score.")]
     private ScoreSystem _scoreSystem;
- 
+
 
 
     private void OnTriggerEnter(Collider collider)
     {
-       //if the object that the player is colliding with is a collectable.
-        if(collider.gameObject.TryGetComponent<Collectable>(out Collectable item))
+        //if the object that the player is colliding with is a collectable.
+        if (collider.gameObject.TryGetComponent<Collectable>(out Collectable item))
         {
             //then
             //increase the current score by the cash value of item
             _scoreSystem.IncreaseScore(item.Data.CashValue);
-            
+
             //remove the item in the scene
             Destroy(item.gameObject);
 
-           
+
         }
 
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
         //if the object that the player is colliding with is an enemy
-        else if(collider.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy"))
         {
             //then
             //reduce the current score by the reduce score value.
             _scoreSystem.ReduceScore();
-         
-
         }
-            
-
-        
     }
-
-
-
-
-
-
-
-
-
-
-
 }
