66f4e7e62394db42d5691952a4873941eb1fefee
Jurhell Hughes <somethinghughes@gmail.com>
2024-05-22 12:54:16 -0500

    feat: Made it so patrolling cop will chase player if in range

--- Assets/_Project/Characters/Cop/CopPatrolBehavior.cs
+++ Assets/_Project/Characters/Cop/CopPatrolBehavior.cs
@@ -11,6 +11,9 @@ public class CopPatrolBehavior : MonoBehaviour
     [SerializeField, Tooltip("What the cop will chase once it enters it's radius. (The Player)")]
     private GameObject _target;
 
+    [SerializeField, Min(0.5f), Tooltip("How big the patrolling cop's player detection radius is.")]
+    private float _copDetectionRadius = 10;
+
     [Space]
 
     [SerializeField, Tooltip("Stores the places on the map the cop will move to. Can be objects with mesh renders and colliders turned off.")]
@@ -54,17 +57,20 @@ public class CopPatrolBehavior : MonoBehaviour
         //Check if player has moved far enough away from agent when seeking
         if (_agentIsSeeking && _cop.remainingDistance > 15)
         {
-            //Reset and transition agent path
+            //Reset agent path back to patrol path
             _cop.ResetPath();
             _cop.path = _patrolPath;
 
+            //Tell console that agent is not seeking
             _agentIsSeeking = false;
 
-            TransitionTo(EState.WANDER);
+            //Reset idle time and have agent idle
+            _idleTime = 0;
+            TransitionTo(EState.IDLE);
         }
 
         //Checking if player is within agro range before seeking
-        if (RadiusCheck())
+        if (RadiusCheck() && _patrolStarted)
             TransitionTo(EState.PURSUE);
 
         //If statements that check agent's current state
@@ -72,6 +78,7 @@ public class CopPatrolBehavior : MonoBehaviour
         {
             _idleTime += Time.deltaTime;
 
+            //Stop idling after 2 seconds
             if (_idleTime >= 2)
                 TransitionTo(EState.PATROL);
 
@@ -100,8 +107,9 @@ public class CopPatrolBehavior : MonoBehaviour
         if (!_agentIsSeeking)
             return;
 
-        //Seeking 
-        _cop.destination = _target.transform.position;
+        Pursue();
+
+        //Smoothing agent's velocity
         _cop.transform.position = Vector3.SmoothDamp(_cop.transform.position, _cop.nextPosition, ref _velocity, 0.05f);
     }
 
@@ -123,16 +131,13 @@ public class CopPatrolBehavior : MonoBehaviour
     {
         float seekMagni = (_target.transform.position - _cop.transform.position).magnitude;
 
-        if (seekMagni >= _cop.radius)
+        if (seekMagni <= _cop.radius * _copDetectionRadius)
         {
             _agentIsSeeking = true;
             return true;
         }
         else
-        {
-            TransitionTo(EState.IDLE);
             return false;
-        }
     }
 
     //Checks if agent has reached it's destination and isn't moving
@@ -184,10 +189,35 @@ public class CopPatrolBehavior : MonoBehaviour
         _cop.destination = _navPoints[_navIter].transform.position;
         _cop.transform.position = Vector3.SmoothDamp(_cop.transform.position, _cop.nextPosition, ref _velocity, 0.05f);
 
+        //Storing current patrol path
         _patrolPath = _cop.path;
 
         _inMotion = true;
         _hasReachedPath = false;
+    }
+
+    private void Pursue()
+    {
+        //Storing direction to target
+        Vector3 targetDirection = _target.transform.position - _cop.transform.position;
+
+        //Storing variables needed for pursue beahvior
+        float relativeHead = Vector3.Angle(_cop.transform.forward, _cop.transform.TransformVector(_target.transform.forward));
+        float toTarget = Vector3.Angle(_cop.transform.forward, _cop.transform.TransformVector(targetDirection));
+
+        //Checking if to target and relative heading are within needed parameters and player has adequate speed for the behavior
+        if ((toTarget > 90 && relativeHead < 20) || _target.GetComponent<Input>().MaxSpeed < 0.01f)
+        {
+            //Running seek behavior if not
+            _cop.destination = _target.transform.position;
+            return;
+        }
+
+        //Storing force to have agent look ahead of player
+        float lookAhead = targetDirection.magnitude / (_cop.speed + _target.GetComponent<Input>().MaxSpeed);
+
+        //Having agent pursue 
+        _cop.destination = _target.transform.position + _target.transform.forward * lookAhead;
     }
 
     private void Wander()
