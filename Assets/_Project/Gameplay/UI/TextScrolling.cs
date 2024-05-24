d56abac94db0f927b04274b76351ac975fc772e8
Jurhell Hughes <somethinghughes@gmail.com>
2024-05-24 11:48:23 -0500

    feat: Created a script to scroll text in the main menu

--- Assets/_Project/Gameplay/UI/TextScrolling.cs
+++ Assets/_Project/Gameplay/UI/TextScrolling.cs
@@ -1,18 +1,17 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextScrolling : MonoBehaviour
 {
+    [SerializeField, Tooltip("The Rect Transform the textmesh.")]
+    private RectTransform _text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _text.DOMoveX(5.0f, 5.0f).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
     }
 }
