9c52dea12d00441f11ab81c193bc31eaf758ba1c
Jurhell Hughes <somethinghughes@gmail.com>
2024-06-03 12:04:40 -0500

    feat: Added a Time's Up Script

---
+++ Assets/_Project/Gameplay/UI/EndState.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EndState : MonoBehaviour
+{
+    [SerializeField, Tooltip("The UI canvas for the ending screen.")]
+    private GameObject _endScreenUI;
+
+    private GameObject _ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EndingCall()
+    {
+        _endScreenUI.SetActive(true);
+        Time.timeScale = 0f;
+        //Do more stuff
+    }
+}
