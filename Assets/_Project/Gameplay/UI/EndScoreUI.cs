f041785653367ec26b2b897d99ce1d63fee3e5c3
Jurhell Hughes <somethinghughes@gmail.com>
2024-06-07 12:15:08 -0500

    feat: Began working on End screen score ui

---
+++ Assets/_Project/Gameplay/UI/EndScoreUI.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndScoreUI : MonoBehaviour
+{
+    [SerializeField, Tooltip("How many seconds the UI will wait before displaying the score.")]
+    private float _waitTime = 3;
+
+    [Space]
+
+    [SerializeField, Tooltip("The TextMesh that will display the player's final score on the end screen.")]
+    private TextMeshProUGUI _endScoreDisplay;
+
+    [SerializeField, Tooltip("The game's Score System, holds the cash amount that will go into Score Display.")]
+    private ScoreSystem _playerScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Wait(() => ShowResults(), _waitTime));
+    }
+
+    private void ShowResults()
+    {
+        _endScoreDisplay.enabled = true;
+
+        //Do stuff
+    }
+
+    private IEnumerator Wait(Action callback, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        callback();
+    }
+}
